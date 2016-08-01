11cd1dfe80c0dccacf65c8edf11b6c144ac388ce
nabster <reg@n-d-p.ru>
2016-08-01 14:17:30 +0300

    Обновил файл запуска

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
  BINDIR=$(dirname "$(readlink -fn "$0")")
  cd "$BINDIR"
-java -Xincgc -Xms2048M -Xmx2048M -XX:MaxPermSize=256M -Dfile.encoding=UTF-8 -jar spigot.jar
+java -Xmx1024M -Xms1024M -jar spigot.jar
 exit 0
